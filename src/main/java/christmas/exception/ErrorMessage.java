5dc292e7cfa9cebdeff140fe88c63687cb659587
pokycookie <0cookieboy0@gmail.com>
2023-11-10 01:52:55 +0900

    feat: ErrorMessage enum 생성

---
+++ src/main/java/christmas/exception/ErrorMessage.java
@@ -0,0 +1,19 @@
+package christmas.exception;
+
+public enum ErrorMessage {
+    WRONG_DATE("유효하지 않은 날짜입니다."),
+    WRONG_ORDER("유효하지 않은 주문입니다.");
+
+    private final String PREFIX = "[ERROR]";
+    private final String SUFFIX = "다시 입력해 주세요.";
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return String.join(" ", PREFIX, message, SUFFIX);
+    }
+}
