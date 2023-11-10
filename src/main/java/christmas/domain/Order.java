a4c4756208534e9fd00b6021eb77b0a5b01a765f
pokycookie <0cookieboy0@gmail.com>
2023-11-10 17:21:27 +0900

    feat: Order 클래스 생성
    - 주문 메뉴와 수량을 저장
    - 주문 가격을 반환하는 메서드
    - 주문 메뉴의 타입을 반환하는 메서드
    - 테스트 코드 작성

---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+import christmas.menu.Menu;
+import christmas.menu.MenuType;
+
+public class Order {
+    private final Menu menu;
+    private final int count;
+
+    private Order(Menu menu, int count) {
+        this.menu = menu;
+        this.count = count;
+    }
+
+    public static Order create(Menu menu, int count) {
+        return new Order(menu, count);
+    }
+
+    public int getPrice() {
+        return menu.getPrice() * count;
+    }
+
+    public MenuType getType() {
+        return menu.getType();
+    }
+}
---
+++ src/test/java/christmas/domain/OrderTest.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+import christmas.menu.Menu;
+import christmas.menu.MenuType;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrderTest {
+    @Test
+    void checkOrderPrice() {
+        assertThat(Order.create(Menu.SEAFOOD_PASTA, 2).getPrice())
+                .isEqualTo(70000);
+    }
+
+    @Test
+    void checkOrderType() {
+        assertThat(Order.create(Menu.ICE_CREAM, 1).getType())
+                .isEqualTo(MenuType.DESSERT);
+    }
+}
