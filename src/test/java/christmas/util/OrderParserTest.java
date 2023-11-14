59550e627b133213cd6a5d8a0e20c7a35c53e197
pokycookie <0cookieboy0@gmail.com>
2023-11-14 14:20:32 +0900

    test: OrderParser 테스트 코드 리팩토링 및 추가

--- src/test/java/christmas/util/OrderParserTest.java
+++ src/test/java/christmas/util/OrderParserTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class OrderParserTest {
-    @DisplayName("주문이 제대로 파싱되는지 확인")
+    @DisplayName("parseOrderOrThrow 동작 확인")
     @ParameterizedTest
     @MethodSource("parseOrderResult")
     void checkParseOrderOrThrow(String orderSequence, List<OrderDTO> answer) {
@@ -28,13 +28,13 @@ public class OrderParserTest {
                         new OrderDTO("제로콜라", 2),
                         new OrderDTO("티본스테이크", 5)
                 )),
-                Arguments.of("타파스 - 1, 제로콜라- 2, 티본 스테이크 -6", List.of(
+                Arguments.of("타파스 - 1, 제로콜라- 2, 티본 스테이크 -5", List.of(
                         new OrderDTO("타파스", 1),
                         new OrderDTO("제로콜라", 2),
-                        new OrderDTO("티본스테이크", 6)
+                        new OrderDTO("티본스테이크", 5)
                 )),
-                Arguments.of("타파스-1", List.of(
-                        new OrderDTO("타파스", 1)
+                Arguments.of("티본스테이크-1", List.of(
+                        new OrderDTO("티본스테이크", 1)
                 )),
                 Arguments.of("해산물파스타-3 , ", List.of(
                         new OrderDTO("해산물파스타", 3)
@@ -44,7 +44,14 @@ public class OrderParserTest {
 
     @DisplayName("잘못된 형식의 주문을 입력하면 예외가 발생")
     @ParameterizedTest
-    @ValueSource(strings = {"타파스: 1, 제로콜라: 2", "타파스-1 제로콜라-2 파스타-3", "타파스 -", " - 3"})
+    @ValueSource(strings = {
+            "타파스: 1, 제로콜라: 2",
+            "타파스-1 제로콜라-2 파스타-3",
+            "타파스, 제로콜라, 파스타",
+            "타파스 1, 제로콜라 2, 파스타 3",
+            "타파스 -",
+            " - 3"
+    })
     void checkParseOrderOrThrowWithWrongPattern(String orderSequence) {
         assertThatThrownBy(() -> OrderParser.parseOrderOrThrow(orderSequence))
                 .isInstanceOf(IllegalArgumentException.class)
